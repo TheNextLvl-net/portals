3e57a3bb9a4bf31cc221cf5e25ea3a5221144e99
david <mail@nonswag.dev>
2025-10-08 16:12:46 +0200

    Added Gradle wrapper and build scripts

---
+++ build.gradle.kts
@@ -0,0 +1,86 @@
+import io.papermc.hangarpublishplugin.model.Platforms
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
+plugins {
+    id("java")
+    id("com.gradleup.shadow") version "9.2.2"
+    id("com.modrinth.minotaur") version "2.+"
+    id("de.eldoria.plugin-yml.paper") version "0.8.0"
+    id("io.papermc.hangar-publish-plugin") version "0.1.3"
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+tasks.compileJava {
+    options.release.set(21)
+}
+
+group = "net.thenextlvl.portals"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.thenextlvl.net/releases")
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    implementation(project(":api"))
+
+    compileOnly("io.papermc.paper:paper-api:1.21.9-R0.1-SNAPSHOT")
+    
+    implementation("net.thenextlvl.core:i18n:3.2.2")
+    implementation("net.thenextlvl.core:paper:2.3.1")
+
+    implementation("org.bstats:bstats-bukkit:3.1.0")
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("portals")
+    relocate("org.bstats", "net.thenextlvl.portals.bstats")
+}
+
+paper {
+    name = "Portals"
+    main = "net.thenextlvl.portals.PortalsPlugin"
+    apiVersion = "1.21.4"
+    description = "Create, delete and manage custom portals"
+    load = BukkitPluginDescription.PluginLoadOrder.STARTUP
+    website = "https://thenextlvl.net"
+    authors = listOf("NonSwag")
+    foliaSupported = true
+}
+
+val versionString: String = project.version as String
+val isRelease: Boolean = !versionString.contains("-pre")
+
+val versions: List<String> = (property("gameVersions") as String)
+    .split(",")
+    .map { it.trim() }
+
+hangarPublish { // docs - https://docs.papermc.io/misc/hangar-publishing
+    publications.register("plugin") {
+        id.set("Portals")
+        version.set(versionString)
+        changelog = System.getenv("CHANGELOG")
+        channel.set(if (isRelease) "Release" else "Snapshot")
+        apiKey.set(System.getenv("HANGAR_API_TOKEN"))
+        platforms.register(Platforms.PAPER) {
+            jar.set(tasks.shadowJar.flatMap { it.archiveFile })
+            platformVersions.set(versions)
+        }
+    }
+}
+
+modrinth {
+    token.set(System.getenv("MODRINTH_TOKEN"))
+    projectId.set("R2dgUCYq")
+    changelog = System.getenv("CHANGELOG")
+    versionType = if (isRelease) "release" else "beta"
+    uploadFile.set(tasks.shadowJar)
+    gameVersions.set(versions)
+    syncBodyFrom.set(rootProject.file("README.md").readText())
+    loaders.addAll((property("loaders") as String).split(",").map { it.trim() })
+}
