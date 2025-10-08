96308d1181156bb788861147396d94815713a13a
david <mail@nonswag.dev>
2025-10-08 16:15:04 +0200

    Added Gradle build script for API module

---
+++ api/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("java")
+    id("java-library")
+    id("maven-publish")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.compileJava {
+    options.release.set(21)
+}
+
+group = rootProject.group
+version = rootProject.version
+
+repositories {
+    mavenCentral()
+    maven("https://repo.thenextlvl.net/releases")
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.9-R0.1-SNAPSHOT")
+}
+
+tasks.withType<Javadoc>().configureEach {
+    val options = options as StandardJavadocDocletOptions
+    options.tags("apiNote:a:API Note:", "implSpec:a:Implementation Requirements:")
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        artifactId = "portals"
+        groupId = "net.thenextlvl"
+        pom.url.set("https://thenextlvl.net/docs/portals")
+        pom.scm {
+            val repository = "TheNextLvl-net/portals"
+            url.set("https://github.com/$repository")
+            connection.set("scm:git:git://github.com/$repository.git")
+            developerConnection.set("scm:git:ssh://github.com/$repository.git")
+        }
+        from(components["java"])
+    }
+    repositories.maven {
+        val branch = if (version.toString().contains("-pre")) "snapshots" else "releases"
+        url = uri("https://repo.thenextlvl.net/$branch")
+        credentials {
+            username = System.getenv("REPOSITORY_USER")
+            password = System.getenv("REPOSITORY_TOKEN")
+        }
+    }
+}
