80fcb73415f812e53923983848d75f76d54e0508
david <mail@nonswag.dev>
2025-10-10 20:02:42 +0200

    Added Metrics shutdown on disable

--- src/main/java/net/thenextlvl/portals/PortalsPlugin.java
+++ src/main/java/net/thenextlvl/portals/PortalsPlugin.java
@@ -2,6 +2,7 @@ package net.thenextlvl.portals;
 
 import net.thenextlvl.portals.listener.PortalListener;
 import net.thenextlvl.portals.portal.PaperPortalProvider;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jspecify.annotations.NullMarked;
@@ -9,6 +10,7 @@ import org.jspecify.annotations.NullMarked;
 @NullMarked
 public final class PortalsPlugin extends JavaPlugin {
     private final PaperPortalProvider portalProvider = new PaperPortalProvider(this);
+    private final Metrics metrics = new Metrics(this, 27514);
 
     public PortalsPlugin() {
         getServer().getServicesManager().register(PortalProvider.class, portalProvider, this, ServicePriority.Highest);
@@ -17,6 +19,11 @@ public final class PortalsPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new PortalListener(this), this);
+    }
+
+    @Override
+    public void onDisable() {
+        metrics.shutdown();
     }
 
     public PaperPortalProvider portalProvider() {
