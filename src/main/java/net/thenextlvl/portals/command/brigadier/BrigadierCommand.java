c86d5b55c997e87430a52f43b5f12333abe8245f
david <mail@nonswag.dev>
2025-10-28 22:14:14 +0100

    Make permission nullable in commands
    - Updated permission field to be nullable
    - Adjusted permission check to handle null values

--- src/main/java/net/thenextlvl/portals/command/brigadier/BrigadierCommand.java
+++ src/main/java/net/thenextlvl/portals/command/brigadier/BrigadierCommand.java
@@ -2,10 +2,13 @@ package net.thenextlvl.portals.command.brigadier;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.command.brigadier.argument.resolvers.ArgumentResolver;
 import net.thenextlvl.portals.PortalsPlugin;
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 
 import java.util.Optional;
 
@@ -13,10 +16,10 @@ import java.util.Optional;
 public abstract class BrigadierCommand {
     protected final PortalsPlugin plugin;
 
-    private final String permission;
+    private final @Nullable String permission;
     private final String name;
 
-    protected BrigadierCommand(PortalsPlugin plugin, String name, String permission) {
+    protected BrigadierCommand(PortalsPlugin plugin, String name, @Nullable String permission) {
         this.plugin = plugin;
         this.permission = permission;
         this.name = name;
@@ -27,7 +30,7 @@ public abstract class BrigadierCommand {
     }
 
     protected boolean canUse(CommandSourceStack source) {
-        return source.getSender().hasPermission(permission);
+        return permission == null || source.getSender().hasPermission(permission);
     }
 
     protected <T> Optional<T> tryGetArgument(CommandContext<CommandSourceStack> context, String name, Class<T> type) {
@@ -38,5 +41,10 @@ public abstract class BrigadierCommand {
                 return Optional.empty();
             throw e;
         }
+    }
+
+    protected <T> Optional<T> resolveArgument(CommandContext<CommandSourceStack> context, String name, Class<? extends ArgumentResolver<T>> type) throws CommandSyntaxException {
+        var resolver = tryGetArgument(context, name, type).orElse(null);
+        return resolver != null ? Optional.of(resolver.resolve(context.getSource())) : Optional.empty();
     }
 }
