26b0dcb947cb884c75b2c4599419105f8d7cc3c8
david <mail@nonswag.dev>
2025-10-28 22:15:47 +0100

    Renamed ActionTypes to match interface name

---
+++ api/src/main/java/net/thenextlvl/portals/action/ActionTypes.java
@@ -0,0 +1,39 @@
+package net.thenextlvl.portals.action;
+
+import net.thenextlvl.portals.Portal;
+import net.thenextlvl.portals.action.teleport.Bounds;
+import org.bukkit.Location;
+import org.jetbrains.annotations.Contract;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @since 0.1.0
+ */
+public sealed interface ActionTypes permits SimpleActionTypes {
+    @Contract(pure = true)
+    static ActionTypes types() {
+        return SimpleActionTypes.INSTANCE;
+    }
+
+    @Contract(pure = true)
+    ActionType<String> connect();
+
+    @Contract(pure = true)
+    ActionType<String> runCommand();
+
+    @Contract(pure = true)
+    ActionType<String> runConsoleCommand();
+
+    @Contract(pure = true)
+    ActionType<Location> teleport();
+
+    @Contract(pure = true)
+    ActionType<Portal> teleportPortal();
+
+    @Contract(pure = true)
+    ActionType<Bounds> teleportRandom();
+
+    @Contract(pure = true)
+    ActionType<InetSocketAddress> transfer();
+}
