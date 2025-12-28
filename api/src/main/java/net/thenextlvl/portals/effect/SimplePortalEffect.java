eda7983a5f3955b325ce0398dd66458e1fee4d99
david <mail@nonswag.dev>
2025-12-28 19:28:04 +0100

    Implement SimpleWaveEffect class for wave visuals

--- api/src/main/java/net/thenextlvl/portals/effect/SimplePortalEffect.java
+++ api/src/main/java/net/thenextlvl/portals/effect/SimplePortalEffect.java
@@ -1,25 +1,29 @@
 package net.thenextlvl.portals.effect;
 
+import com.google.common.base.Preconditions;
 import org.bukkit.Color;
 import org.bukkit.Particle;
+import org.jspecify.annotations.Nullable;
 
 import java.time.Duration;
 
 public abstract class SimplePortalEffect implements PortalEffect {
-    private final Duration duration;
-    private final Particle particle;
     private final Color color;
-    private final int particleCount;
+    private final Duration duration;
     private final Duration updateInterval;
+    private final Particle particle;
     private final double speed;
+    private final int particleCount;
 
-    protected SimplePortalEffect(Duration duration, Particle particle, Color color, int particleCount, Duration updateInterval, double speed) {
-        this.duration = duration;
-        this.particle = particle;
-        this.color = color;
-        this.particleCount = particleCount;
-        this.updateInterval = updateInterval;
-        this.speed = speed;
+    protected SimplePortalEffect(Builder<?, ?> builder) throws IllegalArgumentException {
+        Preconditions.checkArgument(builder.color != null, "Color cannot be null");
+        Preconditions.checkArgument(builder.particle != null, "Particle cannot be null");
+        this.color = builder.color;
+        this.duration = builder.duration;
+        this.particle = builder.particle;
+        this.particleCount = builder.particleCount;
+        this.speed = builder.speed;
+        this.updateInterval = builder.updateInterval;
     }
 
     @Override
@@ -51,4 +55,51 @@ public abstract class SimplePortalEffect implements PortalEffect {
     public double getSpeed() {
         return speed;
     }
-}
+
+    @SuppressWarnings("unchecked")
+    public abstract static class Builder<T extends PortalEffect, B extends PortalEffect.Builder<T, B>> implements PortalEffect.Builder<T, B> {
+        protected @Nullable Color color = null;
+        protected @Nullable Particle particle = null;
+        protected Duration duration = Duration.ofSeconds(5);
+        protected Duration updateInterval = Duration.ofMillis(50);
+        protected double speed = 1.0;
+        protected int particleCount = 10;
+
+        @Override
+        public B duration(Duration duration) {
+            this.duration = duration;
+            return (B) this;
+        }
+
+        @Override
+        public B particle(Particle particle) {
+            this.particle = particle;
+            return (B) this;
+        }
+
+        @Override
+        public B color(Color color) {
+            this.color = color;
+            return (B) this;
+        }
+
+        @Override
+        public B particleCount(int count) {
+            this.particleCount = count;
+            return (B) this;
+        }
+
+        @Override
+        public B updateInterval(Duration interval) {
+            this.updateInterval = interval;
+            return (B) this;
+        }
+
+        @Override
+        public B speed(double speed) {
+            this.speed = speed;
+            return (B) this;
+        }
+    }
+}   
+    
