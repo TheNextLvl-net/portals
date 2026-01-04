faa3ce7c19a844c3863077453f243c6f4bac3303
david <mail@nonswag.dev>
2026-01-04 10:17:25 +0100

    Enhanced debug paste with logs and metadata

--- src/main/java/net/thenextlvl/portals/plugin/commands/PortalDebugPasteCommand.java
+++ src/main/java/net/thenextlvl/portals/plugin/commands/PortalDebugPasteCommand.java
@@ -6,6 +6,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
@@ -62,13 +63,21 @@ final class PortalDebugPasteCommand extends SimpleCommand {
         var sender = context.getSource().getSender();
 
         var debug = new JsonObject();
+        var logs = new JsonArray();
         var portals = new JsonArray();
+        debug.addProperty("version", plugin.getPluginMeta().getVersion());
+        debug.addProperty("server", plugin.getServer().getName() + " " + plugin.getServer().getVersion());
+        debug.addProperty("uptime", DurationAdapter.toString(Duration.ofMillis(plugin.uptime())));
+        if (plugin.omittedLogs() > 0) logs.add(new JsonPrimitive(
+                "Omitted " + plugin.omittedLogs() + " logs"
+        ));
         plugin.portalProvider().portals.stream()
                 .map(gson::toJsonTree)
                 .forEach(portals::add);
+        plugin.logs().map(JsonPrimitive::new).forEach(logs::add);
         debug.add("config", gson.toJsonTree(plugin.config()));
         debug.add("portals", portals);
-
+        debug.add("logs", logs);
         plugin.bundle().sendMessage(sender, "portal.debug-paste",
                 Placeholder.parsed("debug", gson.toJson(debug)));
         return SINGLE_SUCCESS;
