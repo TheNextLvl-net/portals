45023adcf5122d9342c5f37b511e250ece9a6437
david <mail@nonswag.dev>
2026-02-01 19:54:32 +0100

    Add helper methods to format notification edit commands

--- src/main/java/net/thenextlvl/portals/plugin/commands/notification/NotificationListCommand.java
+++ src/main/java/net/thenextlvl/portals/plugin/commands/notification/NotificationListCommand.java
@@ -1,9 +1,11 @@
 package net.thenextlvl.portals.plugin.commands.notification;
 
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.text.minimessage.tag.resolver.Formatter;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.thenextlvl.portals.Portal;
@@ -14,6 +16,7 @@ import net.thenextlvl.portals.plugin.commands.arguments.NotificationTriggerArgum
 import net.thenextlvl.portals.plugin.commands.brigadier.SimpleCommand;
 import net.thenextlvl.portals.plugin.commands.suggestions.PortalNotificationTriggerSuggestionProvider;
 import net.thenextlvl.portals.plugin.commands.suggestions.PortalWithNotificationSuggestionProvider;
+import net.thenextlvl.portals.view.UnparsedTitle;
 import org.bukkit.command.CommandSender;
 import org.jspecify.annotations.NullMarked;
 
@@ -62,7 +65,7 @@ final class NotificationListCommand extends SimpleCommand {
         final var notifications = portal.getNotifications().findByTrigger(trigger)
                 .map(notification -> plugin.bundle().component("portal.notification.list.entry", sender,
                         Placeholder.parsed("type", notification.type().getName()),
-                        Placeholder.parsed("input", String.valueOf(notification.input()))))
+                        Placeholder.parsed("command", toEditCommand(portal, notification))))
                 .toList();
 
         final var message = notifications.isEmpty()
@@ -74,5 +77,41 @@ final class NotificationListCommand extends SimpleCommand {
                 Placeholder.parsed("trigger", trigger.getName()),
                 Formatter.number("count", notifications.size()),
                 Formatter.joining("notifications", notifications));
+    }
+
+    private String toEditCommand(final Portal portal, final Notification<?> notification) {
+        return "/portal notification set "
+                + portal.getName() + " "
+                + notification.trigger().getName() + " "
+                + notification.type().getName() + " "
+                + toString(notification);
+    }
+
+    private static String toString(final Notification<?> notification) {
+        return switch (notification.input()) {
+            case final UnparsedTitle unparsed -> toString(unparsed);
+            case final Sound sound -> toString(sound);
+            default -> String.valueOf(notification.input());
+        };
+    }
+
+    private static String toString(final UnparsedTitle unparsed) {
+        final var string = StringArgumentType.escapeIfRequired(unparsed.title()) + " "
+                + StringArgumentType.escapeIfRequired(unparsed.subtitle());
+
+        if (unparsed.times() == null) return string;
+
+        final var times = unparsed.times().fadeIn().toMillis() / 1000d + "s "
+                + unparsed.times().stay().toMillis() / 1000d + "s "
+                + unparsed.times().fadeOut().toMillis() / 1000d + "s";
+
+        return string + " " + times;
+    }
+
+    private static String toString(final Sound sound) {
+        return sound.name().asString() + " "
+                + sound.source().name() + " "
+                + sound.volume() + " "
+                + sound.pitch();
     }
 }
