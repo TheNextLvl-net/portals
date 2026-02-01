e6271c0f870dacb89a91053ef58a69ee4db2a396
david <mail@nonswag.dev>
2026-02-01 18:49:17 +0100

    Add JavaDoc to Notification interface

--- api/src/main/java/net/thenextlvl/portals/notification/Notification.java
+++ api/src/main/java/net/thenextlvl/portals/notification/Notification.java
@@ -4,15 +4,46 @@ import net.thenextlvl.portals.Portal;
 import org.bukkit.entity.Entity;
 import org.jetbrains.annotations.Contract;
 
+/**
+ * Represents a notification.
+ *
+ * @param <T> the type of the notification
+ * @since 1.4.0
+ */
 public interface Notification<T> {
+    /**
+     * Gets the trigger for this notification.
+     *
+     * @return the trigger for this notification
+     * @since 1.4.0
+     */
     @Contract(pure = true)
     NotificationTrigger trigger();
 
+    /**
+     * Gets the type of this notification.
+     *
+     * @return the type of this notification
+     * @since 1.4.0
+     */
     @Contract(pure = true)
     NotificationType<T> type();
 
+    /**
+     * Gets the input for this notification.
+     *
+     * @return the input for this notification
+     * @since 1.4.0
+     */
     @Contract(pure = true)
     T input();
 
+    /**
+     * Sends this notification to the given entity.
+     *
+     * @param entity the entity to send the notification to
+     * @param portal the portal that triggered the notification
+     * @since 1.4.0
+     */
     void send(Entity entity, Portal portal);
 }
