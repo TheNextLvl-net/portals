961f8730d378a3bf3b4a10e72786eee527708f2a
david <mail@nonswag.dev>
2026-05-06 13:44:31 +0200

    Improve color argument parsing and suggestions

--- src/main/java/net/thenextlvl/portals/plugin/commands/arguments/ColorArgumentType.java
+++ src/main/java/net/thenextlvl/portals/plugin/commands/arguments/ColorArgumentType.java
@@ -3,24 +3,31 @@ package net.thenextlvl.portals.plugin.commands.arguments;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
-import com.mojang.brigadier.LiteralMessage;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Color;
 import org.jspecify.annotations.NullMarked;
 
-@NullMarked
-public final class ColorArgumentType implements CustomArgumentType.Converted<Color, String> {
-    private static final SimpleCommandExceptionType INVALID_COLOR = new SimpleCommandExceptionType(
-            new LiteralMessage("Expected color in #RRGGBB format")
-    );
+import java.util.concurrent.CompletableFuture;
 
+@NullMarked
+public final class ColorArgumentType implements CustomArgumentType<Color, String> {
     @Override
-    public Color convert(final String nativeType) throws CommandSyntaxException {
-        if (!nativeType.matches("#[0-9a-fA-F]{6}")) throw INVALID_COLOR.create();
-        final var rgb = Integer.parseInt(nativeType.substring(1), 16);
-        return Color.fromRGB(rgb);
+    public Color parse(final StringReader reader) throws CommandSyntaxException {
+        try {
+            return toColor(ArgumentTypes.namedColor().parse(reader));
+        } catch (final CommandSyntaxException e) {
+            return toColor(ArgumentTypes.hexColor().parse(reader));
+        }
+    }
+
+    private Color toColor(final TextColor color) {
+        return Color.fromRGB(color.value());
     }
 
     @Override
@@ -29,11 +36,7 @@ public final class ColorArgumentType implements CustomArgumentType.Converted<Col
     }
 
     @Override
-    public Color parse(final StringReader reader) throws CommandSyntaxException {
-        try {
-            return CustomArgumentType.Converted.super.parse(reader);
-        } catch (final CommandSyntaxException e) {
-            throw INVALID_COLOR.createWithContext(reader);
-        }
+    public <S> CompletableFuture<Suggestions> listSuggestions(final CommandContext<S> context, final SuggestionsBuilder builder) {
+        return ArgumentTypes.namedColor().listSuggestions(context, builder);
     }
 }
